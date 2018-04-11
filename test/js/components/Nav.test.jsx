052eb63ecdd09a03609e7e8f25ec41c8ba3cb85f
Taimoor Khan <taimoor.m.k@live.com>
2018-04-11 16:48:14 +0500

    Nav testing done

--- test/js/components/Nav.test.jsx
+++ test/js/components/Nav.test.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { App } from '../../../src/js/components/App';
+import { Nav } from '../../../src/js/components/Nav';
 
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('App Component', () => {
+describe('Nav Component', () => {
   let getVideos, wrapper;
 
   beforeEach(() => {
     getVideos = jest.fn();
-    wrapper = shallow(<App getVideos={getVideos} />);
+    wrapper = shallow(<Nav getVideos={getVideos} />);
   });
 
   it('renders', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should run getVideos with correct arguments', () => {
-    expect(getVideos.mock.calls[0]).toEqual(['javascript', true]);
+  it('passes on getVideos', () => {
+    expect(getVideos.mock.calls.length).toBe(0);
   });
 });
